2f44ac144842da90d347567731a4161ed7a33e44
Andrey Karamnoff <karamnoff@gmail.com>
2021-07-20 19:10:24 +0300

    Add 'Details' view

--- src/views/Details/Details.jsx
+++ src/views/Details/Details.jsx
@@ -1,8 +1,142 @@
 /* eslint-disable import/no-anonymous-default-export */
 
-import React from 'react';
-import { Details } from './Details.styles.jsx';
+import React, { useEffect, useRef, useState } from 'react';
+import CustomScroll from 'react-custom-scroll';
+import 'react-custom-scroll/dist/customScroll.css';
+import {
+  Details,
+  DetailsTitle,
+  DetailsText as p,
+  DetailsPanel,
+  DetailsPanelInner,
+  DetailsPanelContent,
+  DetailsPanelText,
+  DetailsElements,
+  DetailsElementSperm1,
+  DetailsElementSperm2,
+  DetailsElementSperm3,
+  DetailsElementSperm4,
+  DetailsElementSperm5,
+} from './Details.styles.jsx';
+import sperm1Image from '../../assets/images/details/details__element--sperm1.png';
+import sperm1ImageRetina from '../../assets/images/details/details__element--sperm1@2x.png';
+import sperm2Image from '../../assets/images/details/details__element--sperm2.png';
+import sperm2ImageRetina from '../../assets/images/details/details__element--sperm2@2x.png';
+import sperm3Image from '../../assets/images/details/details__element--sperm3.png';
+import sperm3ImageRetina from '../../assets/images/details/details__element--sperm3@2x.png';
+import sperm4Image from '../../assets/images/details/details__element--sperm4.png';
+import sperm4ImageRetina from '../../assets/images/details/details__element--sperm4@2x.png';
+import sperm5Image from '../../assets/images/details/details__element--sperm5.png';
+import sperm5ImageRetina from '../../assets/images/details/details__element--sperm5@2x.png';
 
 export default () => {
-  return <Details></Details>;
+  const panelContentRef = useRef(null);
+  const panelTextRef = useRef(null);
+  const [hasScroll, setHasScroll] = useState(false);
+
+  useEffect(() => {
+    if (panelContentRef.current && panelTextRef.current) {
+      if (
+        panelTextRef.current.clientHeight > panelContentRef.current.clientHeight
+      ) {
+        setHasScroll(true);
+      }
+    }
+  }, [hasScroll, setHasScroll]);
+  return (
+    <Details className="details">
+      <DetailsElements className="details__elements">
+        <DetailsElementSperm1
+          className="details__element details__element--sperm1"
+          srcSet={`${sperm1Image} 1x, ${sperm1ImageRetina} 2x`}
+          alt="cell"
+          width="1141"
+          height="532"
+        ></DetailsElementSperm1>
+        <DetailsElementSperm2
+          className="details__element details__element--sperm2"
+          srcSet={`${sperm2Image} 1x, ${sperm2ImageRetina} 2x`}
+          alt="cell"
+          width="731"
+          height="342"
+        ></DetailsElementSperm2>
+        <DetailsElementSperm3
+          className="details__element details__element--sperm3"
+          srcSet={`${sperm3Image} 1x, ${sperm3ImageRetina} 2x`}
+          alt="cell"
+          width="414"
+          height="196"
+        ></DetailsElementSperm3>
+        <DetailsElementSperm4
+          className="details__element details__element--sperm4"
+          srcSet={`${sperm4Image} 1x, ${sperm4ImageRetina} 2x`}
+          alt="cell"
+          width="731"
+          height="342"
+        ></DetailsElementSperm4>
+        <DetailsElementSperm5
+          className="details__element details__element--sperm5"
+          srcSet={`${sperm5Image} 1x, ${sperm5ImageRetina} 2x`}
+          alt="cell"
+          width="327"
+          height="158"
+        ></DetailsElementSperm5>
+      </DetailsElements>
+      <DetailsTitle>
+        текст
+        <br />
+        сообщения
+      </DetailsTitle>
+      <DetailsPanel className="panel" hasScroll={hasScroll}>
+        <DetailsPanelInner className="panel__inner" hasScroll={hasScroll}>
+          <CustomScroll allowOuterScroll={true} rtl={true}>
+            <DetailsPanelContent
+              className="panel__content"
+              ref={panelContentRef}
+            >
+              <DetailsPanelText
+                className="panel__text"
+                ref={panelTextRef}
+                hasScroll={hasScroll}
+              >
+                <p>
+                  <b>Сперматозoид</b>&nbsp;(от&nbsp;др.-греч.
+                  &sigma;&pi;έ&rho;&mu;&alpha; род.&nbsp;п.
+                  &sigma;&pi;έ&rho;&mu;&alpha;&tau;&omicron;&sigmaf; здесь
+                  &laquo;сперма&raquo; + &zeta;&omega;ή &laquo;жизнь&raquo; +
+                  &epsilon;ἶ&delta;&omicron;&sigmaf; &laquo;вид, облик&raquo;,
+                  лат. spermatozoon, spermium)&nbsp;&mdash; мужская половая
+                  клетка (гамета) размножающихся посредством оогамии организмов.
+                  Сперматозоиды обычно обладают способностью к&nbsp;активному
+                  движению и&nbsp;служат для оплодотворения женской
+                  гаметы&nbsp;&mdash; яйцеклетки. Обычно они значительно меньше
+                  яйцеклеток, поскольку не&nbsp;содержат столь значительного
+                  количества цитоплазмы и&nbsp;производятся организмом
+                  одновременно в&nbsp;значительном количестве[1].
+                </p>
+                <p>
+                  Типичное строение сперматозоида отражает форму общего предка
+                  животных и&nbsp;грибов: одноклеточный ядерный организм,
+                  передвигающийся за&nbsp;счёт жгутика в&nbsp;задней части,
+                  используя его подобно хвосту. Обширная группа происходящих
+                  от&nbsp;него организмов включает в&nbsp;себя животных,
+                  большинство грибов и&nbsp;некоторые группы протистов
+                  и&nbsp;называется кланом заднежгутиковых. Большинство других
+                  эукариот со&nbsp;жгутиками имеют их&nbsp;в&nbsp;передней
+                  части.
+                </p>
+                <p>
+                  В&nbsp;широком смысле слова по&nbsp;традиции сперматозоидами
+                  иногда называют мужские половые клетки также у&nbsp;растений,
+                  к&nbsp;ним применяют также термины спермии или антерозоиды
+                  (их&nbsp;применяют также к&nbsp;традиционно сближавшимся
+                  с&nbsp;растениями грибам).
+                </p>
+              </DetailsPanelText>
+            </DetailsPanelContent>
+          </CustomScroll>
+        </DetailsPanelInner>
+      </DetailsPanel>
+    </Details>
+  );
 };
--- src/views/Details/Details.styles.jsx
+++ src/views/Details/Details.styles.jsx
@@ -1,10 +1,185 @@
 import styled from 'styled-components/macro';
+import SuperQuery from '@themgoncalves/super-query';
+import detailsBgImage from '../../assets/images/details/details__bg.png';
+import detailsBgImageRetina from '../../assets/images/details/details__bg@2x.png';
 
 export const Details = styled.section`
   position: relative;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-flow: column wrap;
+  justify-content: flex-start;
+  align-items: flex-start;
   width: inherit;
   height: inherit;
+  padding-top: 130px;
+  padding-right: 130px;
+  padding-bottom: 80px;
+  padding-left: 65px;
+  background: url(${detailsBgImage}) #fff no-repeat center center / contain;
+  overflow: hidden;
+
+  ${SuperQuery().minResolution.of('192dpi').css`
+    background-image: url(${detailsBgImageRetina});
+  `}
+`;
+
+export const DetailsElements = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`;
+
+export const DetailsElement = styled.img`
+  position: absolute;
+  display: flex;
+  mix-blend-mode: multiply;
+`;
+
+export const DetailsElementSperm1 = styled(DetailsElement)`
+  top: 98px;
+  left: 366px;
+`;
+
+export const DetailsElementSperm2 = styled(DetailsElement)`
+  top: 95px;
+  left: 585px;
+`;
+
+export const DetailsElementSperm3 = styled(DetailsElement)`
+  top: 75px;
+  left: 710px;
+`;
+
+export const DetailsElementSperm4 = styled(DetailsElement)`
+  bottom: 15px;
+  right: -595px;
+`;
+
+export const DetailsElementSperm5 = styled(DetailsElement)`
+  bottom: 95px;
+  right: -190px;
+`;
+
+export const DetailsTitle = styled.h2`
+  display: flex;
+  flex-flow: row wrap;
+  margin: 0;
+  margin-bottom: 45px;
+  padding: 0;
+  font-family: 'DINPro', Arial, sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 27px;
+  line-height: 1.2;
+  color: #242424;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+`;
+
+export const DetailsPanel = styled.div.attrs(({ className }) => {
+  return className;
+})`
+  position: relative;
+  display: block;
+
+  &.panel {
+    width: auto;
+    height: initial;
+
+    .rcs-custom-scroll {
+      .rcs-inner-container {
+        direction: rtl;
+      }
+    }
+
+    .rcs-custom-scrollbar {
+      width: 20px;
+    }
+
+    .rcs-inner-handle {
+      display: inline-block;
+      background-image: linear-gradient(to right, #96436f 0%, transparent 45%),
+        linear-gradient(to left, #e6a9c5 0%, transparent 45%),
+        linear-gradient(to top, #e6a9c5 0%, transparent 5%),
+        linear-gradient(to bottom, #96436f 0%, transparent 5%);
+      background-color: #ff6a9f;
+      border-radius: 20px;
+      cursor: pointer;
+    }
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 30px;
+    left: 10px;
+    display: ${(props) => (props.hasScroll ? 'block' : 'none')};
+    width: 5px;
+    height: calc(100% - 60px);
+    border-radius: 5px;
+    background-color: #242424;
+    transition: all 0.3s ease-in-out;
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: ${(props) => (props.hasScroll ? '60px' : 0)};
+    display: block;
+    width: ${(props) => (props.hasScroll ? 'calc(100% - 60px)' : '100%')};
+    height: 100%;
+    border-radius: 40px;
+    background-color: #ffffff;
+    transition: all 0.3s ease-in-out;
+  }
+`;
+
+export const DetailsPanelInner = styled.div`
+  position: relative;
+  padding: 30px;
+  padding-left: ${(props) => (props.hasScroll ? 0 : null)};
+
+  &::after {
+    content: '';
+    z-index: 10;
+    position: absolute;
+    top: 0;
+    left: 60px;
+    display: ${(props) => (props.hasScroll ? 'block' : 'none')};
+    width: calc(100% - 60px);
+    height: 100%;
+    border-radius: 40px;
+    background-image: linear-gradient(to top, #ffffff 0%, transparent 25%);
+    transition: all 0.3s ease-in-out;
+  }
+`;
+
+export const DetailsPanelContent = styled.div`
+  max-height: 385px;
+`;
+
+export const DetailsPanelText = styled.div`
+  z-index: 5;
+  position: relative;
+  display: inline-block;
+  font-family: 'DINPro', Arial, sans-serif;
+  font-weight: 400;
+  font-style: normal;
+  font-size: 22px;
+  line-height: 30px;
+  color: #242424;
+  transition: all 0.3s ease-in-out;
+
+  p {
+    margin: 0;
+    padding: 0;
+    padding-left: ${(props) => (props.hasScroll ? '90px' : null)};
+    transition: inherit;
+  }
+
+  b {
+    font-family: 'DINPro-Bold', Arial, sans-serif;
+    font-weight: 700;
+  }
 `;
